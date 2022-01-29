683236759ae3674539bd9da7222f5ec756e8de66
code21112 <code21112@protonmail.com>
2022-01-29 16:04:55 +0100

    Changed server.js commenting if statement with client/build

--- server/server.js
+++ server/server.js
@@ -21,13 +21,13 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "./../client/build")));
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static(path.join(__dirname, "./../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./../client/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./../client/build/index.html"));
+//   });
+// }
 
 // DB
 mongoose
